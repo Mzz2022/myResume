a34ca199f481d022f316ddaec3f2df1648569204
Mzz2022 <1556435206@qq.com>
2024-12-05 16:15:08 +0800

    模板初始化

---
+++ Fif-resume/src/App.tsx
@@ -0,0 +1,38 @@
+import mySvg from './assets/dui.svg'
+import './App.css'
+
+const dui=<img src={mySvg} alt='my svg' style={{ width: '30px', height: 'auto' }}></img>
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <div className='resume-container'>
+          {dui}
+          <h2>1</h2>
+          <h2>2</h2>
+          <h2>3</h2>
+          <h2>4</h2>
+          <h2>5</h2>
+          <h2>6</h2>
+          <h2>7</h2>
+          <h2>8</h2>
+          <h2>9</h2>
+          <h2>10</h2>
+          <h2>11</h2>
+          <h2>12</h2>
+          <h2>13</h2>
+          <h2>14</h2>
+          <h2>15</h2>
+          <h2>16</h2>
+          <h2>17</h2>
+          <h2>18</h2>
+          <h2>19</h2>
+          <h2>20</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
